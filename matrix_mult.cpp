4676afca9cab5b392d99cd286c93f716d7ea388c
Shih Feng Lin <noahleft@gmail.com>
2013-12-29 00:57:33 +0800

    OpenCL initial context and get devices list

--- matrix_mult.cpp
+++ matrix_mult.cpp
@@ -79,11 +79,27 @@ int main() {
         return 0;
     }
 
+    //Create OpenCL context
+    cl_context_properties prop[] = { CL_CONTEXT_PLATFORM, reinterpret_cast<cl_context_properties>(platforms[0]), 0 };
+    context = clCreateContextFromType(prop, CL_DEVICE_TYPE_DEFAULT, NULL, NULL, NULL);
+    if (context == 0) {
+        std::cerr << "Can't create OpenCL context\n";
+        return 0;
+    }
 
-
-
-
-
+    //Get devices list
+    size_t cb;
+    clGetContextInfo(context, CL_CONTEXT_DEVICES, 0, NULL, &cb);
+    std::vector<cl_device_id> devices( cb/ sizeof(cl_device_id));
+    clGetContextInfo(context, CL_CONTEXT_DEVICES, cb, &devices[0], 0);
+    
+    //Get devices info
+    clGetDeviceInfo(devices[0], CL_DEVICE_NAME, 0, NULL, &cb);
+    std::string devname;
+    devname.resize(cb);
+    clGetDeviceInfo(devices[0], CL_DEVICE_NAME, cb, &devname[0], 0);
+    std::cout << "Device: " << devname.c_str() << "\n";
+    
 
 
    /* Check result */
